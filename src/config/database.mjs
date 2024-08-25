8eb235586442ea8059f7b1e59a0bae5d458fd57b
Juan Adhiasta Pratama <juanadhiastapersonal@gmail.com>
2024-08-25 19:21:07 +0700

    fix .env

--- src/config/database.mjs
+++ src/config/database.mjs
@@ -11,7 +11,7 @@ dotenv.config();
 
 const db = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
-    dialect: 'mysql',
+    dialect: process.env.DB_DIALECT,
     port: process.env.DB_PORT
 });
 
